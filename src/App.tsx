8dd39e9b5177b9c9d598e33baad057a841b55471
Musabii Mambetov <81331273+musabiii@users.noreply.github.com>
2022-06-10 12:13:37 +0300

    supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
+import { supabase } from './utils/sapabaseClient';
 import './App.css';
 
 function App() {
+
+  const handleClick = (e:MouseEvent<HTMLButtonElement>)=> {
+    e.preventDefault();
+
+    console.log('click');
+    getSupabase();
+  }
+
+  async function getSupabase() {
+    let { data: accessories, error } = await supabase
+    .from('accessories')
+    console.log(accessories);
+  }
+
   return (
     <div className="App">
       Hello
+      <button onClick={handleClick}>sclick</button>
     </div>
   );
 }
